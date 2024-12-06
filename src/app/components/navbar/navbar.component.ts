c6d8304fe84efa6c69290aa2907aa22a2acf1916
unknown <rpcanova@hotmail.com>
2024-12-05 22:25:41 -0300

    feature: added hamburger menu and responsiveness to navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrls: ['./navbar.component.css', './navbar.responsive.component.css']
 })
 export class NavbarComponent {
 
