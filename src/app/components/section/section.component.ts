fa96f0dcdac4f81a0e3ab1e47aa12f67a8bf0d55
unknown <rpcanova@hotmail.com>
2024-12-24 00:02:59 -0300

    added responsiveness to section component on home page

--- src/app/components/section/section.component.ts
+++ src/app/components/section/section.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-section',
   templateUrl: './section.component.html',
-  styleUrl: './section.component.css'
+  styleUrls: ['./section.component.css', './section.responsive.component.css']
 })
 export class SectionComponent {
 
